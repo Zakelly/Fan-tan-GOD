2c2ea0b2ecddd0e96959d33d6887fe6f65b0942e
周昊宇 <zhy.zbklqx@Gmail.com>
2016-04-09 09:27:43 +0800

    .

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,21 @@
+<?php namespace App\Http\Controllers;
+
+use App\Post;
+
+class PostController extends Controller {
+
+	/**
+	 * Create a new controller instance.
+	 *
+	 * @return void
+	 */
+	public function __construct()
+	{
+		// $this->middleware('auth');
+	}
+	
+	public function create()
+	{
+		
+	}
+}
