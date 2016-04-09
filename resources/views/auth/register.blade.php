52fa73a7a4b5b76e37569f0bd1541412a62fa0d6
Zakelly <Zakelly@163.com>
2016-04-09 09:08:02 +0800

    user

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -24,17 +24,9 @@
 						<div class="form-group">
 							<label class="col-md-4 control-label">Name</label>
 							<div class="col-md-6">
-								<input type="text" class="form-control" name="name" value="{{ old('name') }}">
+								<input type="text" class="form-control" name="username" value="{{ old('username') }}">
 							</div>
 						</div>
-
-						<div class="form-group">
-							<label class="col-md-4 control-label">E-Mail Address</label>
-							<div class="col-md-6">
-								<input type="email" class="form-control" name="email" value="{{ old('email') }}">
-							</div>
-						</div>
-
 						<div class="form-group">
 							<label class="col-md-4 control-label">Password</label>
 							<div class="col-md-6">
